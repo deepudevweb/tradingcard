866b66f92c88447aa68a2ebad60fa2bdd8f88879
deepu <deepuprajapati0846@gmail.com>
2023-07-07 19:49:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import '../src/card.css';
 import Cardhover from './Card'
 import '../src/app.css';
-import backview from '../src/backview.mp4';
+// import backview from '../src/backview.mp4';
+import tunnel from '../src/tunnel.mp4'
 function CardView() {
   return (
      <div className='container1'>
       {<video className='video1' autoPlay loop muted>
-         <source src={backview} type='video/mp4' />
+         <source src={tunnel} type='video/mp4' />
       </video>}
       <Cardhover />
     </div>
--- src/Card.js
+++ src/Card.js
@@ -34,12 +34,14 @@ function Cardhover() {
                         <img className="logo" src={trade} alt="" />
                         <h1 className="heading">Xml<span>trader</span> <br /> </h1>
                         <h4 className="contact">ph : {data.phone}</h4>
-                        <div className="name"> <h2 >{data.name.title} <span> {data.name.first} </span>  <span>{data.name.last}</span></h2> </div>
+                        <div className="name"> <h2 >{data.name.title}. <span> {data.name.first} </span>  <span>{data.name.last}</span></h2> </div>
 
                     </div>
                     <div className="mid">
                         <div className="box"> <h5>Gender <br /><span>{data.gender}</span></h5></div>
+                       <div className="line"><hr/></div> 
                         <div className="box">  <h5>Age<br /><span>{data.dob.age}</span></h5></div>
+                       <div className="line2" ><hr/></div>
                         <div className="box">  <h5>Address<br /><span>{data.location.city}/{data.location.country} {data.location.state}</span></h5></div>
                     </div>
                     <div className="down">
